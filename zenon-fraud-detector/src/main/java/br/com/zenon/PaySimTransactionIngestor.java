0e456298d949ba74ed4e6bf5358f74e3ac9d21aa
Felipe Alencar <feealencarr@gmail.com>
2026-08-27 22:46:43 -0300

    Feat: implementado validações para importação do arquivo

--- zenon-fraud-detector/src/main/java/br/com/zenon/PaySimTransactionIngestor.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/PaySimTransactionIngestor.java
@@ -11,10 +11,10 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PaySimTransactionIngestor {
-
     private final File fileToIngest;
     private static final int DEFAULT_NUMBER_OF_LINES = 1000;
     private boolean readEntireFile = false;
@@ -22,47 +22,6 @@ public class PaySimTransactionIngestor {
 
     public PaySimTransactionIngestor(File fileToIngest) {
         this.fileToIngest = fileToIngest;
-    }
-
-    public List<Transaction> read() throws IOException {
-        return this.readEntireFile
-                ? this.readEntireFile(this.fileToIngest)
-                : this.readFirstXLines(this.fileToIngest, this.numberOfLines);
-    }
-
-    private List<Transaction> readEntireFile(File file) throws IOException {
-        String line;
-        List<Transaction> transactions = new ArrayList<>();
-
-        BufferedReader reader = this.readFile(file);
-        reader.readLine(); //ignoring header line
-        while ((line = reader.readLine()) != null){
-            transactions.add(PaySimTransactionMapper.fromCsvLine(line));
-        }
-        reader.close();
-        return  transactions;
-    }
-
-    private List<Transaction> readFirstXLines( File file, int numberOfLinesToRead) throws IOException {
-        String line;
-        List<Transaction> transactions = new ArrayList<>();
-
-        BufferedReader reader = this.readFile(file);
-        reader.readLine(); //ignoring header line;
-        int count = 0;
-        while ((line = reader.readLine()) != null){
-            if(count == numberOfLinesToRead){
-                break;
-            }
-            transactions.add(PaySimTransactionMapper.fromCsvLine(line));
-            count++;
-        }
-        reader.close();
-        return transactions;
-    }
-
-    private BufferedReader readFile(File file) throws FileNotFoundException {
-        return new BufferedReader(new FileReader(file));
     }
 
     public PaySimTransactionIngestor entireFile() {
@@ -77,10 +36,70 @@ public class PaySimTransactionIngestor {
         return this;
     }
 
+    public List<Transaction> read() throws IOException {
+        return this.readEntireFile
+                ? this.readEntireFile(this.fileToIngest)
+                : this.readFirstXLines(this.fileToIngest, this.numberOfLines);
+    }
+
+    private List<Transaction> readEntireFile(File file) throws IOException {
+        List<Transaction> transactions = new ArrayList<>();
+        try(BufferedReader reader = this.readFile(file)){
+            String header = reader.readLine();
+            PaySimTransactionValidator validator = new PaySimTransactionValidator(header);
+
+            String register;
+
+            while ((register = reader.readLine()) != null){
+                try{
+                    String[] splittedRegister = register.split(",", -1);
+                    validator.assertValidLine(splittedRegister);
+                    transactions.add(PaySimTransactionMapper.fromCsvLine(splittedRegister));
+                } catch (Exception e) {
+                    System.err.printf("Error: %s | %s  -> %s%n", register, e.getClass().getName(),e.getMessage());
+                }
+            }
+
+            return  transactions;
+        }
+    }
+
+    private List<Transaction> readFirstXLines( File file, int numberOfLinesToRead) throws IOException {
+        List<Transaction> transactions = new ArrayList<>();
+        try(BufferedReader reader = this.readFile(file)){
+            String header = reader.readLine();
+            PaySimTransactionValidator validator = new PaySimTransactionValidator(header);
+
+            String register;
+            int count = 0;
+            while ((register = reader.readLine()) != null){
+                if(count == numberOfLinesToRead){
+                    break;
+                }
+
+                try{
+                    String[] splittedRegister = register.split(",", -1);
+                    validator.assertValidLine(splittedRegister);
+                    transactions.add(PaySimTransactionMapper.fromCsvLine(splittedRegister));
+                } catch (Exception e) {
+                    System.err.printf("Error: %s | %s  -> %s%n", register, e.getClass().getName(),e.getMessage());
+                }
+
+                count++;
+
+            }
+
+            return transactions;
+        }
+    }
+
+    private BufferedReader readFile(File file) throws FileNotFoundException {
+        return new BufferedReader(new FileReader(file));
+    }
+
     static class PaySimTransactionMapper {
 
-        public static Transaction fromCsvLine(String line){
-            String[] splittedLine = line.split(",");
+        public static Transaction fromCsvLine(String[] splittedLine){
             return  new Transaction(
                     Integer.parseInt(splittedLine[0]),
                     TransactionType.valueOf(splittedLine[1]),
@@ -91,5 +110,64 @@ public class PaySimTransactionIngestor {
                     "1".equals(splittedLine[10])
             );
         };
+    }
+
+    static class PaySimTransactionValidator{
+        private final String[] headers;
+
+        public PaySimTransactionValidator(String headers){
+            this.headers = headers.split(",", -1);
+        };
+
+        public void assertValidLine(String[] values){
+
+
+            for(int i = 0; i < values.length; i++){
+               isNotEmpty(this.headers[i], values[i]);
+            }
+
+            isPositive(headers[0], Integer.parseInt(values[0]));
+            isValidEnum(headers[1], values[1], TransactionType.class);
+            isGreaterOrEquals(headers[2], new BigDecimal(values[2]), new BigDecimal("0.00"));
+            isGreaterOrEquals(headers[4], new BigDecimal(values[4]), new BigDecimal("0.00"));
+            isGreaterOrEquals(headers[5], new BigDecimal(values[5]), new BigDecimal("0.00"));
+            isGreaterOrEquals(headers[7], new BigDecimal(values[7]), new BigDecimal("0.00"));
+            isGreaterOrEquals(headers[8], new BigDecimal(values[8]), new BigDecimal("0.00"));
+            isIntegerIn(headers[9], Integer.parseInt(values[9]), List.of(0,1));
+            isIntegerIn(headers[10], Integer.parseInt(values[10]), List.of(0,1));
+        }
+
+
+        private static <T extends Enum<T> >void isValidEnum(String field, String value, Class<T> clazz){
+            try {
+                Enum.valueOf(clazz, value);
+            }catch (Exception e){
+                throw new IllegalArgumentException(String.format("%s should be one of them %s : %s", field, Arrays.toString(clazz.getEnumConstants()), value));
+            }
+        }
+
+        private  static void isNotEmpty(String field, String value){
+            if(value.isBlank()){
+                throw new IllegalArgumentException(String.format("%s should not be empty : %s", field, value));
+            }
+        }
+
+        private static  void isPositive(String field, Number value){
+            if(value.longValue() <= 0){
+                throw new IllegalArgumentException(String.format("%s should be positive : %s", field, value));
+            }
+        }
+
+      private static void isIntegerIn(String field, Integer value, List<Integer> list ){
+            if(!list.contains(value)){
+                throw new IllegalArgumentException(String.format("%s should be in %s: %s", field, list, value));
+            }
+      }
+
+        private static void isGreaterOrEquals(String field, BigDecimal value, BigDecimal reference){
+            if(value.compareTo(reference) <0 ){
+                throw new IllegalArgumentException(String.format("%s should greater than or equals %s: %s", field, reference, value));
+            }
+        }
     }
 }
--- zenon-fraud-detector/src/main/java/br/com/zenon/ZenonFraudDetectorApplication.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/ZenonFraudDetectorApplication.java
@@ -1,6 +1,19 @@
 package br.com.zenon;
 
+import java.io.IOException;
+import java.nio.file.Path;
+
 public class ZenonFraudDetectorApplication {
-    public static void main(String[] args){
+    void main() throws IOException {
+        //Path path = Path.of("data/PS_20174392719_1491204439457_log.csv");
+        Path path = Path.of("data/paysim_with_bad_data.csv");
+        long start = System.currentTimeMillis();
+        new PaySimTransactionIngestor(path.toFile())
+                .entireFile()
+                .read()
+                .forEach(IO::println);
+
+        IO.println(System.currentTimeMillis() - start + " ms");
+
     }
 }
