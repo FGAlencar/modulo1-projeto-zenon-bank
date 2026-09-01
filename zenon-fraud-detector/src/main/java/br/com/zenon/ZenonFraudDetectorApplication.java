259bfac655bc66a92842c6c6649edace899f9810
Felipe Alencar <feealencarr@gmail.com>
2026-08-31 23:45:30 -0300

    Feat: melhorias de lógica

--- zenon-fraud-detector/src/main/java/br/com/zenon/ZenonFraudDetectorApplication.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/ZenonFraudDetectorApplication.java
@@ -1,17 +1,18 @@
 package br.com.zenon;
 
+import br.com.zenon.representation.transaction.Transaction;
+
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.List;
 
 public class ZenonFraudDetectorApplication {
     void main() throws IOException {
-        //Path path = Path.of("data/PS_20174392719_1491204439457_log.csv");
-        Path path = Path.of("data/paysim_with_bad_data.csv");
+        Path path = Path.of("data/PS_20174392719_1491204439457_log.csv");
+        //Path path = Path.of("data/paysim_with_bad_data.csv");
         long start = System.currentTimeMillis();
-        new PaySimTransactionIngestor(path.toFile())
-                .entireFile()
-                .read()
-                .forEach(IO::println);
+        PaySimTransactionIngestor ingestor = new PaySimTransactionIngestor(path.toFile());
+        List<Transaction> transactionList = ingestor.numberOfLines(10).start();
 
         IO.println(System.currentTimeMillis() - start + " ms");
 
